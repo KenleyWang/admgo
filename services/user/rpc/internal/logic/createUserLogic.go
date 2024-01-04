585cb6e0ab02f77362efe40fa56f7307fc1f8ea5
Kenley <1161723373@qq.com>
2024-01-04 22:08:28 +0800

    优化user代码

---
+++ services/user/rpc/internal/logic/createUserLogic.go
@@ -0,0 +1,37 @@
+package logic
+
+import (
+	"context"
+	"github.com/admgo/admgo/services/user/rpc/internal/db/models"
+
+	"github.com/admgo/admgo/services/user/rpc/internal/svc"
+	"github.com/admgo/admgo/services/user/rpc/pb"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type CreateUserLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewCreateUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *CreateUserLogic {
+	return &CreateUserLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+// 创建用户
+func (l *CreateUserLogic) CreateUser(in *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+	// todo: add your logic here and delete this line
+
+	err := l.svcCtx.UserModel.Insert(l.ctx, &models.User{
+		UserName: in.UserName,
+		Name:     in.Name,
+	})
+
+	return &pb.CreateUserResponse{}, err
+}
