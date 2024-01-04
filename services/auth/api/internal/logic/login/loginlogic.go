3bedc782168d9634344665d70dcaaa5bfb292cc4
Kenley <1161723373@qq.com>
2024-01-04 22:08:14 +0800

    优化auth代码

--- services/auth/api/internal/logic/login/loginlogic.go
+++ services/auth/api/internal/logic/login/loginlogic.go
@@ -2,12 +2,9 @@ package login
 
 import (
 	"context"
-	"database/sql"
-	"github.com/admgo/admgo/services/user/rpc/user"
-	"time"
-
 	"github.com/admgo/admgo/services/auth/api/internal/svc"
 	"github.com/admgo/admgo/services/auth/api/internal/types"
+	"github.com/admgo/admgo/services/user/rpc/user"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -27,26 +24,11 @@ func NewLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LoginLogic 
 }
 
 func (l *LoginLogic) Login(req *types.LoginReq) (resp *types.LoginRsp, err error) {
-	// todo: add your logic here and delete this line
 
-	type User struct {
-		Id       int64          `db:"id"`
-		Name     sql.NullString `db:"name"`     // The username
-		Password string         `db:"password"` // The user password
-		Mobile   string         `db:"mobile"`   // The mobile phone number
-		Gender   string         `db:"gender"`   // gender,male|female|unknown
-		Nickname string         `db:"nickname"` // The nickname
-		Type     int64          `db:"type"`     // The user type, 0:normal,1:vip, for test golang keyword
-		CreateAt sql.NullTime   `db:"create_at"`
-		UpdateAt time.Time      `db:"update_at"`
-	}
+	a, err := l.svcCtx.UserRPC.FindSingleUser(l.ctx, &user.FindSingleUserRequest{UserId: 1})
 
-	var result User
-
-	// Raw SQL
-	l.svcCtx.DB.Raw("SELECT name FROM users WHERE name = ?", "users").Scan(&result)
-	_, err := l.svcCtx.UserRPC.FindSingleUser(l.ctx, &user.FindSingleUserRequest{UserId: 1})
-
-	return
+	return &types.LoginRsp{
+		Msg: a.UserName,
+	}, err
 
 }
