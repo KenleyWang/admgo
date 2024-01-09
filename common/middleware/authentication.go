fdd0b63c5f34eec66a1f874a18abda19854dd22c
Kenley <1161723373@qq.com>
2024-01-09 22:55:23 +0800

    添加鉴权中间件

---
+++ common/middleware/authentication.go
@@ -0,0 +1,44 @@
+package middleware
+
+import (
+	"github.com/admgo/admgo/pkg/errorx"
+	"github.com/zeromicro/go-zero/core/stores/redis"
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"net/http"
+)
+
+type (
+	AuthenticationOptions struct {
+		RedisConf redis.RedisConf
+	}
+	AuthenticationMiddleware struct {
+		sessionRedis *redis.Redis
+	}
+)
+
+func NewAuthenticationMiddleware(c redis.RedisConf) *AuthenticationMiddleware {
+	return &AuthenticationMiddleware{
+		sessionRedis: redis.MustNewRedis(c, redis.WithPass(c.Pass)),
+	}
+}
+
+func (m *AuthenticationMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		cookie, err := r.Cookie("user_token")
+		if err != nil {
+			httpx.Error(w, errorx.ServerErr)
+			return
+		}
+		isExist, err := m.sessionRedis.Exists("/auth/api/user_token/" + cookie.Value)
+		if err != nil {
+			httpx.Error(w, errorx.ServerErrForRedis)
+			return
+		}
+		if !isExist {
+			httpx.Error(w, errorx.NoLogin)
+			return
+		}
+		next(w, r)
+	}
+}
