e5ff99986a8a950d3fec466bc1c2aae9fb70e498
Kenley <1161723373@qq.com>
2024-01-09 22:56:45 +0800

    重新生成auth代码

---
+++ services/user/rpc/internal/logic/findSingleUserByUserIDLogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"github.com/admgo/admgo/services/user/rpc/internal/svc"
+	"github.com/admgo/admgo/services/user/rpc/pb"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type FindSingleUserByUserIDLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewFindSingleUserByUserIDLogic(ctx context.Context, svcCtx *svc.ServiceContext) *FindSingleUserByUserIDLogic {
+	return &FindSingleUserByUserIDLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+// 根据用户ID查找指定用户
+func (l *FindSingleUserByUserIDLogic) FindSingleUserByUserID(in *pb.FindSingleUserByUserIDRequest) (*pb.FindSingleUserByUserIDResponse, error) {
+
+	return &pb.FindSingleUserByUserIDResponse{}, nil
+}
