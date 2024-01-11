99cdebd51d135f5dc5fda9b676798ce0c90c0c64
Kenley <1161723373@qq.com>
2024-01-11 16:23:05 +0800

    完善鉴权中间件

--- common/middleware/authentication.go
+++ common/middleware/authentication.go
@@ -1,7 +1,9 @@
 package middleware
 
 import (
+	"context"
 	"github.com/admgo/admgo/pkg/errorx"
+	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/stores/redis"
 	"github.com/zeromicro/go-zero/rest/httpx"
 	"net/http"
@@ -24,21 +26,32 @@ func NewAuthenticationMiddleware(c redis.RedisConf) *AuthenticationMiddleware {
 
 func (m *AuthenticationMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-
+		reqCtx := r.Context()
 		cookie, err := r.Cookie("user_token")
 		if err != nil {
+			logx.Error(err)
 			httpx.Error(w, errorx.NoLogin)
 			return
 		}
-		isExist, err := m.sessionRedis.Exists("/user/rest/user_token/" + cookie.Value)
+		isExist, err := m.sessionRedis.ExistsCtx(reqCtx, "/user/rest/session/"+cookie.Value)
 		if err != nil {
+			logx.Error(err)
 			httpx.Error(w, errorx.ServerErrForRedis)
 			return
 		}
 		if !isExist {
+			logx.Errorf("%s", err)
 			httpx.Error(w, errorx.NoLogin)
 			return
 		}
-		next(w, r)
+		val, err := m.sessionRedis.HgetCtx(r.Context(), "/user/rest/session/"+cookie.Value, "user_id")
+		if err != nil {
+			httpx.Error(w, errorx.ServerErrForRedis)
+			return
+		}
+		ctx := context.WithValue(reqCtx, "UserID", val)
+		newReq := r.WithContext(ctx)
+
+		next(w, newReq)
 	}
 }
