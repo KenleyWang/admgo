8b00a8e9b16479096262581b7f9780650acc6b6d
Kenley <1161723373@qq.com>
2024-01-18 13:54:31 +0800

    添加错误状态码

--- services/user/rpc/internal/code/code.go
+++ services/user/rpc/internal/code/code.go
@@ -5,4 +5,5 @@ import "github.com/admgo/admgo/pkg/errorx"
 var (
 	UserEmpty                   = errorx.NewRpcStatus(120101, "no user")
 	IncorrectUserNameORPassword = errorx.NewRpcStatus(120102, "incorrect user name or password")
+	MysqlErr                    = errorx.NewRpcStatus(120102, "internal error")
 )
