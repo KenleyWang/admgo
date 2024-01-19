84a741c5b592124369fd5611ef4d7b3587f9670d
Kenley <1161723373@qq.com>
2024-01-19 22:00:22 +0800

    修改config文件，添加DB的配置

--- services/cmdb/rpc/internal/config/config.go
+++ services/cmdb/rpc/internal/config/config.go
@@ -1,7 +1,11 @@
 package config
 
-import "github.com/zeromicro/go-zero/zrpc"
+import (
+	db "github.com/admgo/admgo/pkg/db/config"
+	"github.com/zeromicro/go-zero/zrpc"
+)
 
 type Config struct {
 	zrpc.RpcServerConf
+	DB db.Config
 }
