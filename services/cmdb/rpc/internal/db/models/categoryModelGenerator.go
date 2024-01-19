5e20125b2ed5842b73efd0c328c06501826fd3dc
Kenley <1161723373@qq.com>
2024-01-19 21:59:24 +0800

    添加ORM模型

---
+++ services/cmdb/rpc/internal/db/models/categoryModelGenerator.go
@@ -0,0 +1,50 @@
+package models
+
+import (
+	"context"
+	"gorm.io/gorm"
+)
+
+type (
+	categoryModel interface {
+		Insert(ctx context.Context, data *Category) error
+		FindOne(ctx context.Context, id int64) (*Category, error)
+		Update(ctx context.Context, data *Category) error
+	}
+
+	defaultCategoryModel struct {
+		db        *gorm.DB
+		tableName string
+	}
+
+	Category struct {
+		BaseModel
+		Name         string `gorm:"column:name;type:varchar(30);not null;comment:显示名"`
+		CategoryName string `gorm:"column:category_name;type:varchar(30);not null;unique;comment:类别名(唯一)"`
+	}
+)
+
+func (emp Category) TableName() string {
+	return ModelPrefix + "_" + "categorys"
+}
+
+func newCategoryModel(conn *gorm.DB) *defaultCategoryModel {
+	return &defaultCategoryModel{
+		db:        conn,
+		tableName: ModelPrefix + "_" + "categorys",
+	}
+}
+
+func (m *defaultCategoryModel) Insert(ctx context.Context, data *Category) error {
+	return m.db.WithContext(ctx).Create(data).Error
+}
+
+func (m *defaultCategoryModel) FindOne(ctx context.Context, id int64) (*Category, error) {
+	var result Category
+	err := m.db.WithContext(ctx).Where("id = ?", id).First(&result).Error
+	return &result, err
+}
+
+func (m *defaultCategoryModel) Update(ctx context.Context, data *Category) error {
+	return m.db.WithContext(ctx).Save(data).Error
+}
---
+++ services/cmdb/rpc/internal/db/models/modelModelGenerator.go
@@ -0,0 +1,50 @@
+package models
+
+import (
+	"context"
+	"gorm.io/gorm"
+)
+
+type (
+	modelModel interface {
+		Insert(ctx context.Context, data *Model) error
+		FindOne(ctx context.Context, id int64) (*Model, error)
+		Update(ctx context.Context, data *Model) error
+	}
+
+	defaultModelModel struct {
+		db        *gorm.DB
+		tableName string
+	}
+
+	Model struct {
+		BaseModel
+		Name      string `gorm:"column:name;type:varchar(30);not null;comment:显示名"`
+		ModelName string `gorm:"column:model_name;type:varchar(30);not null;unique;comment:模型名(唯一)"`
+	}
+)
+
+func (emp Model) TableName() string {
+	return ModelPrefix + "_" + "models"
+}
+
+func newModelModel(conn *gorm.DB) *defaultModelModel {
+	return &defaultModelModel{
+		db:        conn,
+		tableName: ModelPrefix + "_" + "models",
+	}
+}
+
+func (m *defaultModelModel) Insert(ctx context.Context, data *Model) error {
+	return m.db.WithContext(ctx).Create(data).Error
+}
+
+func (m *defaultModelModel) FindOne(ctx context.Context, id int64) (*Model, error) {
+	var result Model
+	err := m.db.WithContext(ctx).Where("id = ?", id).First(&result).Error
+	return &result, err
+}
+
+func (m *defaultModelModel) Update(ctx context.Context, data *Model) error {
+	return m.db.WithContext(ctx).Save(data).Error
+}
